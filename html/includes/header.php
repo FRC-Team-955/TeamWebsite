6fe32d98eebc9d281b2dc04127b40c481aaf5a6a
matthberg <matthberg@gmail.com>
2015-01-20 20:28:36 -0800

    migration, maybe

---
+++ html/includes/header.php
@@ -0,0 +1,85 @@
+<!-- Bootstrap core CSS -->
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+        <!-- Custom styles for this template -->
+        <link href="css/customStyles.css" rel="stylesheet">
+        <script src="js/customBehaviors.js" type="text/javascript"></script>
+    </head>
+    <!-- Navigation -->
+    <body id="rO"><!-- #rO is for having our styles override bootstrap.-->
+        <div class="navbar-wrapper">
+            <div class="container-fluid">
+                <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+                    <div class="container">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#"><h1>CV Robotics</h1></a>
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                <span class="sr-only">Toggle navigation</span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                                <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div id="navbar" class="navbar-collapse collapse">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#">Home</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">Structure</a></li>
+                                        <li><a href="#">Mentors</a></li>
+                                        <li><a href="#">Past Robots</a></li>
+                                        <li><a href="#">Website</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Media</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Photos</a></li>
+                                        <li><a href="#">Videos</a></li>
+                                        <li><a href="#">Press</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Resources</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Parent Resources</a></li>
+                                        <li><a href="#">Student Resources</a></li>
+                                        <li><a href="#">Safety</a></li>
+                                        <li><a href="#">Calendar</a></li>
+                                        <li><a href="#">Parent Handbook</a></li>
+                                        <li class="jim"><a href="#"><img src="images/jimHalfHead.png" alt="Coach's Corner"></a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Support</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">Donations</a></li>
+                                        <li><a href="#">Fundraising</a></li>
+                                        <li><a href="#">Sponsors</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">FIRST</a>
+                                    <ul class="dropdown-menu" role="menu">
+                                        <li><a href="#">About FIRST</a></li>
+                                        <li><a href="http://www.usfirst.org/" target="_blank">US FIRST Homepage</a></li>
+                                        <li><a href="http://www.usfirst.org/roboticsprograms/fll" target="_blank">Lego League</a></li>
+                                        <li><a href="http://www.usfirst.org/roboticsprograms/ftc" target="_blank">Tech Challenge</a></li>
+                                        <li><a href="http://www.usfirst.org/roboticsprograms/frc" target="_blank">Robotics Challenge</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="http://cvhs.csd509j.net" target="_blank">CVHS</a></li><!--http://cvhs.csd509j.net-->
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
---
+++ html/pages/template.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>
+            Crescent Valley FIRST Robotics
+        </title>
+        <div id="rO">
+          <?php include "../includes/header.php"; ?> <!-- This will include everything before .bodyText -->
+          <?php include "../includes/carousel.php" ?>
+          <div class="bodyText container-fluid">
+          <div class="row">
+          </div>
+        </div> <!-- End .bodyText -->
+      </div>
+    </body>
+</html>
