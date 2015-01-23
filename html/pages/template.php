dd405f011e624c0faec88c210feeb8babe5d973c
matthberg <matthberg@gmail.com>
2015-01-22 18:57:14 -0800

    something risky's going on here...

--- html/pages/template.php
+++ html/pages/template.php
@@ -3,12 +3,10 @@
         <title>
             Crescent Valley FIRST Robotics
         </title>
-        <div id="rO">
           <?php include "../includes/header.php"; ?> <!-- This will include everything before .bodyText -->
           <div class="bodyText container-fluid">
           <div class="row">
           </div>
         </div> <!-- End .bodyText -->
-      </div>
     </body>
 </html>
