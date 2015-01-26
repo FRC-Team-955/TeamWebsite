3898bc3dd9c27a08dfab1830a7659efb5161fd80
matthberg <matthberg@gmail.com>
2015-01-25 16:20:17 -0800

    favicon change?

--- html/includes/header.php
+++ html/includes/header.php
@@ -3,7 +3,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" href="../images/favicon.png">
+        <link rel="icon" href="../images/tablogo.png">
         <link href="../css/bootstrap.css" rel="stylesheet">
         <link href="../css/bootstrap-theme.css" rel="stylesheet">
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
