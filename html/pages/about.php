2ddeca17ea9d19b44841f3bcc17656f77af16919
matthberg <matthberg@gmail.com>
2015-01-25 17:41:19 -0800

    first full width thing?

--- html/pages/about.php
+++ html/pages/about.php
@@ -13,8 +13,20 @@
       </title>
       <?php include "../includes/header.php"; ?> <!-- This will include everything before .bodyText -->
       <div class="bodyText container-fluid">
-        <div class="row" id="team"><h2>Who we are</h2>
-        </div>
+        <div class="row crossScreenImg" id="team"> <!-- set background image by css with the linking id -->
+                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                    <h2 class="titleL darkBG">Who we are</h2>
+                    <ul class="introUL">
+                      <li><b>Robot Name:</b> Based on the mythical Titans</li>
+                      <li><b>Rookie Year:</b> 2002</li>
+                      <li><b>FRC Team Number:</b> 955</li>
+                      <li><b>Team Size:</b> 40+</li>
+                      <li><b>Team Colors:</b> Maroon & Gold</li>
+                      <li><b>Location:</b> Crescent Valley High School in Corvallis, Oregon</li>
+                      <li><b>Coach:</b> Jim Bowey</li>
+                    </ul>
+                </section>
+            </div>
         <div class="row" id="FIRST"><h2></h2>
         </div>
         <div class="row" id="structure"><h2></h2>
--- html/pages/template.php
+++ html/pages/template.php
@@ -5,8 +5,38 @@
         </title>
           <?php include "../includes/header.php"; ?> <!-- This will include everything before .bodyText -->
           <div class="bodyText container-fluid">
-          <div class="row">
-          </div>
+
+            <!--2/3text, 1/3image-->
+            <div class="row">
+                <section class="roboR col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <img src="" alt="" class="img-circle fullW"/>
+                </section>
+                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                    <h2 class="titleR"></h2>
+                    <p></p>
+                </section>
+            </div>
+
+            <!--1/3image, 2/3text-->
+            <div class="row">
+                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                    <h2 class="titleL"></h2>
+                    <p></p>
+                </section>
+                <section class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                    <img src="" alt="" class="img-circle fullW"/>
+                </section>
+            </div>
+
+            <!--fullImage, 2/3text-->
+            <div class="row crossScreenImg"> <!-- set background image by css with the linking id -->
+                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                    <h2 class="titleL"></h2>
+                    <p></p>
+                </section>
+            </div>
+
+
         </div> <!-- End .bodyText -->
     </body>
 </html>
