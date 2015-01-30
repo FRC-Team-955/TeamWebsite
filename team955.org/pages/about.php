0e3a10d60b774ea5f26492c229b3915c6d95402c
unknown <matthberg@gmail.com>
2015-01-29 18:16:18 -0800

    added Content

--- team955.org/pages/about.php
+++ team955.org/pages/about.php
@@ -17,9 +17,9 @@
                 <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                     <h2 class="titleC darkBG">Who we are</h2>
         <p class="crossScreenImgCols">
-        <b>Robot Name:</b> Based on the mythical Titans<br/>
+        <b>Robot Name:</b> Based on the <a href="http://en.wikipedia.org/wiki/Titan_(mythology)">mythical Titans</a><br/>
                       <b>Rookie Year:</b> 2002<br/>
-                      <b>FRC Team Number:</b> 955<br/>
+                      <b><abbr title="FIRST Robotics Competition">FRC</abbr> Team Number:</b> 955<br/>
                       <b>Team Size:</b> 40+<br/>
                       <b>Team Colors:</b> Maroon & Gold<br/>
                       <b>Location:</b> Crescent Valley High School in Corvallis, Oregon<br/>
@@ -27,6 +27,16 @@
                         </p>
                 </section>
         </div>
+		<div class="row">
+			<section class="roboR col-xs-12 col-sm-4 col-md-4 col-lg-4">
+				<img src="..images/pagePhotos/image3.jpg" alt="There was a great, inspirational picture here" class="img-circle fullW"/>
+			</section>
+			<section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+				<h2 class="titleR"></h2>
+				<p>The Crescent Valley High School Robotics Team was founded in September 2002 by engineering teacher Mark Baldwin and twelve students. Our team (<abbr title="FIRST Robotics Competition">FRC</abbr> Team 955) is part of the NASA sponsored <abbr title="For Inspiration and Recognition of Science and Technology">FIRST</abbr> program. Each year we compete in the worldwide FRC challenge.</p>
+				<p>Since our founding we have grown into more than just a robotics team. We have become a family of over 40 diverse students connected by a shared passion for science, technology, engineering, art, and mathematics (STEAM). Our current coach, Jim Bowey, leads this group of students, mentors, parents, and business partners in the pursuit of our shared passion. </p>
+			</section>
+		</div>
         </div>
         <div class="bodyText container-fluid">
         <div class="row" id="FIRST"><h2></h2>
