467b4ce1bfc5449486a199366c0ea52fa913e37c
matthberg <matthberg@gmail.com>
2015-01-29 20:16:04 -0800

    images all done?

--- team955.org/includes/endDoc.php
+++ team955.org/includes/endDoc.php
@@ -2,6 +2,6 @@
     <!-- Bootstrap core JavaScript
 ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/customBehaviors.js"></script>
