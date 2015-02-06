51c2d024326d2289b5406c136c8fded355d3ee55
Aditya Sriram <adiram18@gmail.com>
2015-02-05 19:07:34 -0800

    hellos, david

--- team955.org/js/customBehaviors.js
+++ team955.org/js/customBehaviors.js
@@ -25,15 +25,5 @@ $(document).ready(function(){
       }
     }
   });
-  var $root = $('html, body');
-$('a').click(function() {
-    var href = $.attr(this, 'href');
-    $root.animate({
-        scrollTop: $(href).offset().top
-    }, 500, function () {
-        window.location.hash = href;
-    });
-    return false;
-});
 });/*end doc.ready*/
 
