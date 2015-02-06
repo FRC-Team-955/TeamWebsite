c381243e19caebeeae8811a4178dd5d60123bdc8
Aditya Sriram <adiram18@gmail.com>
2015-02-05 18:11:20 -0800

    maybe did the scroll to thing right??? copied and pasted off of stack overflow, so who knows

--- team955.org/js/customBehaviors.js
+++ team955.org/js/customBehaviors.js
@@ -27,9 +27,7 @@ $(document).ready(function(){
   });
 });/*end doc.ready*/
 
-function scrollToThis(thing){ /*input "top" for scroll to top */
-  if (thing=="top"){
-    thing="body";
-  }
-  $("html,body").animate({ scrollTop: thing.offset().top }, 200);
-};
+$('a[href*=#]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
