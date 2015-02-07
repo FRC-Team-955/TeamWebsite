af2ed60ae686d752659e2b9506e504ee75fa507f
matthberg <matthberg@gmail.com>
2015-02-06 18:29:43 -0800

    adding about first

--- team955.org/pages/about.php
+++ team955.org/pages/about.php
@@ -63,6 +63,52 @@
                 </section>
             </div>
         </div>
+
+        <div class="subpageBox">
+            <div class="row crossScreenImg" id="FIRST"> <!-- set background image by css with the linking id -->
+                <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12 centerV">
+                    <h2 class="titleC darkBG">FIRST Robotics Programs</h2>
+                    <a href="http://www.usfirst.org/roboticsprograms/frc" target="_blank"><img src="../images/brands/FRCicon.png" class="img-circle col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="FIRST Robotics Competition"></a>
+                    <a href="http://www.usfirst.org/roboticsprograms/ftc" target="_blank"><img src="../images/brands/FTCicon.png" class="img-circle col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="FIRST Tech Challenge"></a>
+                    <a href="http://www.usfirst.org/roboticsprograms/fll" target="_blank"><img src="../images/brands/FLLicon.png" class="img-circle col-xs-4 col-sm-4 col-md-4 col-lg-4" alt="FIRST Lego League"></a>
+                    <h2 class="headerNB">CV Robotics is a <abbr title="FIRST Robotics Competition">FRC</abbr> team that was founded in 2002.</h3>
+                </section>
+            </div>
+        <div class="row">
+          <section class="roboR col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <img src="" alt="A General FRC Photo" class="imgBorder fullW"/>
+          </section>
+          <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+            <p>The FIRST Robotics Competition (FRC) is an international varsity sport of the mind designed to help high-school-aged young people discover how interesting and rewarding the life of engineers and researchers can be. The FIRST Robotics Competition tests the ability of teams and their mentors to complete a challenge in a six-week time frame using a common set of rules. Teams build robots from industrial-grade parts and enter them in competitions designed by Dean Kamen, Dr. Woodie Flowers, and a committee of engineers and other professionals. FIRST redefines winning for these students because they are rewarded for more than just their robot's ability to win. Not only are teams recognized for excellence and creativity in robot design, but they must also demonstrate team spirit, involvement in their communities, gracious professionalism, maturity, and the ability to overcome obstacles. Scoring the most points is a secondary goal. Winning means building skills and partnerships that last. For more information on FIRST, <a href="http://www.usfirst.org/">visit their website.</a></p>
+          </section>
+        </div>
+        <div class="row">
+          <section class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <img src="../images/pagePhotos/image16.jpg" alt="Manufacturing is done by a joint effort of mentors and students" class="imgBorder fullW"/>
+          </section>
+          <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+            <p></p>
+            <p></p>
+          </section>
+        </div>
+        <div class="row">
+                <section class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <h2 class="titleC">Goals of Ours</h2>
+                </section>
+                <section class=" col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                    <p>We constantly strive to increase the amount of time we spend volunteering, primarily by going to elementary schools to actively engage younger students in science and technology education. This extends to middle schools as well. This is a way to spread the FIRST message while recruiting inspired students for future years. We highly value promoting enthusiasm for science and technology in younger students.</p>
+                </section>
+                <section class="quarterRow col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                      <p>A goal of ours is also to streamline fundraising methods that allow the team to work towards providing funds for our program while still allowing students to focus on participating in robotics. We hope that by establishing annual community fundraisers, such as Mother’s Day flower sales, our program will become sustainable and decrease the overall time we spend fundraising to allow for more robotics activities.</p>
+                </section>
+                <section class="quarterRow col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                      <p>We aim to increase the number of partnerships within the local community to promote investment in FIRST and its programs. This will allow the team to exert less effort in fundraising, and put a larger amount of effort into community outreach programs, like Da Vinci Days. Such partnerships link us to our community, and maintaining these partnerships increases the sustainability of our program in future years.</p>
+                </section>
+                <section class="quarterRow col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                      <p>As well as raise the current level of awareness of all sub-team activities within our team. This will allow our team to better function as a whole, and create less miscommunication between sub-teams, like coordinating team activities that involve all members; including fundraisers, or even competition deadlines.</p>
+                </section>
+            </div>
+        </div>-->
         <div class="bodyText container-fluid">
         <div class="row" id="FIRST"><h2></h2>
         </div>
