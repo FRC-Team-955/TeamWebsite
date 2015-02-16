cc5d38681cca5946f240adc9e8b7c0b23a8dd26b
matthberg <matthberg@gmail.com>
2015-02-15 18:14:59 -0800

    might work from home

--- team955.org/pages/support.php
+++ team955.org/pages/support.php
@@ -9,6 +9,19 @@
           Crescent Valley FIRST Robotics
       </title>
       <?php include "../includes/header.php"; ?> <!-- This will include everything before .bodyText -->
-
+      <div class="subpageBox">
+          <div class="row crossScreenImg" id="donations"> <!-- set background image by css with the linking id -->
+            <section class="col-md-12 centerV">
+              <h2 class="titleC darkBG"></h2>
+            </section>
+          </div>
+          <div class="row">
+            <section class="roboR col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <img src="../images/pagePhotos/" alt="" class="imgBorder fullW"/>
+            </section>
+            <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+            </section>
+          </div>
+        </div>
       <?php include "../includes/endDoc.php"; ?>
 </html>
