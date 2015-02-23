1070ecccef413f7cb9c5373b81c8c3e44b9f8ea8
unknown <matthberg@gmail.com>
2015-02-22 16:49:02 -0800

    added a bit of support, sorry for slowness

--- team955.org/pages/support.php
+++ team955.org/pages/support.php
@@ -12,7 +12,18 @@
       <div class="subpageBox">
           <div class="row crossScreenImg" id="donations"> <!-- set background image by css with the linking id -->
             <section class="col-md-12 centerV">
-              <h2 class="titleC darkBG"></h2>
+              <h2 class="titleC darkBG">Donations</h2>
+            </section>
+            <section 
+          </div>
+          <div class="row">
+            <section class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+              <img src="../images/pagePhotos/" alt="" class="imgBorder fullW"/>
+            </section>
+            <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+              <p>It's not about the robot. The robotics program at Crescent Valley High School is about teamwork and building a family. It's about learning that hard work and long hours can lead to success. It's about learning that failure is part of pushing yourself to try new things and learn. It's about making sure that everyone contributes and feels valuable. The robot is just the vehicle.</p>
+              <q>Robotics goes beyond friendship - we're an extended family. We meet people with the same interests and we learn from each other - and it's fun. We learn how to pull it all together to build something REAL and compete in an awesome event. What's not to love about Robotics?
+-Aaron Celeste, Class of 2015</q>
             </section>
           </div>
           <div class="row">
@@ -20,6 +31,7 @@
               <img src="../images/pagePhotos/" alt="" class="imgBorder fullW"/>
             </section>
             <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+              
             </section>
           </div>
         </div>
