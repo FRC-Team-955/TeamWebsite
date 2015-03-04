a79aa9d160dd5a38f773203fd442bcb21ed828f7
matthberg <matthberg@gmail.com>
2015-03-03 18:52:44 -0800

    just trying it out...

--- team955.org/pages/resources.php
+++ team955.org/pages/resources.php
@@ -13,14 +13,8 @@
         <div class="subpageBox">
           <div class="row crossScreenImg" id="Calendar"> <!-- set background image by css with the linking id -->
             <section class="col-md-12 centerV">
-              <h2 class="titleC darkBG"></h2>
-            </section>
-          </div>
-          <div class="row">
-            <section class="roboR col-xs-12 col-sm-4 col-md-4 col-lg-4">
-              <img src="../images/pagePhotos/" alt="" class="imgBorder fullW"/>
-            </section>
-            <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+              <h2 class="titleC darkBG">Calendar</h2>
+          <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cv.raiderbot%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FLos_Angeles" style="border-width:0 " width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
             </section>
           </div>
         </div>
