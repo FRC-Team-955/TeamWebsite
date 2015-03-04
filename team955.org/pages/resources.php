c3dd7f059e77fcc2b78fb6e12dad7623e15bc62d
matthberg <matthberg@gmail.com>
2015-03-03 19:00:29 -0800

    added different way of doing it all

--- team955.org/pages/resources.php
+++ team955.org/pages/resources.php
@@ -14,7 +14,7 @@
           <div class="row crossScreenImg" id="Calendar"> <!-- set background image by css with the linking id -->
             <section class="col-md-12 centerV">
               <h2 class="titleC darkBG">Calendar</h2>
-          <iframe src="https://www.google.com/calendar/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cv.raiderbot%40gmail.com&amp;color=%232952A3&amp;ctz=America%2FLos_Angeles" style="border-width:0 " width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+          <?php include "../includes/calendar.php"; ?>
             </section>
           </div>
         </div>
