a2d6d5c7e48e821b97d52a0d7309890f2134b637
ChromitionAngel <milofarseer@comcast.net>
2016-03-01 20:01:10 -0800

    hopeful time zone change

--- team955.org/pages/calendar.php
+++ team955.org/pages/calendar.php
@@ -16,7 +16,7 @@
             <section class="col-md-12 centerV">
               <h2 class="titleC darkBG">Calendar</h2>
               <section class="col-md-offset-2 col-md-8">
-                  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=emsnt3l7kg15cfrj1n9tk875j0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FNew_York" style="border-width:0" width="100%" height="360px" frameborder="0" scrolling="no"></iframe>
+                  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=emsnt3l7kg15cfrj1n9tk875j0%40group.calendar.google.com&amp;color=%23711616&amp;ctz=America%2FPortland" style="border-width:0" width="100%" height="360px" frameborder="0" scrolling="no"></iframe>
               </section>
             </section>
           </div>
