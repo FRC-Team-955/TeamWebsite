f34c60cfb113218fa3cea40c9aa0e575d4868392
ChromitionAngel <milofarseer@comcast.net>
2016-03-02 11:02:59 -0800

    changed 1 to 2

--- team955.org/js/customBehaviors.js
+++ team955.org/js/customBehaviors.js
@@ -49,7 +49,7 @@ function e(){
     var images = ["webmaster.jpg", "swagmaster2.JPG"];
     $(".imgBorder.fullW").each(function(){
         $(this).attr("src",path+images[Math.floor(Math.random()*2)]);
-        $(this).attr("src", path+''+images[1]);
+        $(this).attr("src", path+''+images[2]);
     });
 };
 a=0;
