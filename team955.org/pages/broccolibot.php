3570e73fdd519d0ee47f0a63f5e8c0185540625c
ChromitionAngel <milofarseer@comcast.net>
2017-11-09 19:31:25 -0800

    updated Broc Bot page

--- team955.org/pages/broccolibot.php
+++ team955.org/pages/broccolibot.php
@@ -11,32 +11,47 @@
                     <section class="col-md-12">
                         <h2 class="titleC darkBG">BroccoliBot</h2>
                     </section>
-                    <section class="col-md-offset-3 col-md-6">
-                        <div class="col-md-6">
-                            ...
-                        </div>
-                        <div class="col-md-6">
-                            ...  
-                        </div>
-                    </section>
                 </div>
             </div> 
             
             <div class="row">
-                <section class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                    <!--<img src="..." alt="" class="imgBorder fullW"/>-->
+                <section class="col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                    <img src="..." alt="" class="imgBorder fullW"/>
                 </section>
-                <section class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+                <section class="col-xs-12 col-sm-6 col-md-6 col-lg-8">
+                    <p>
+                    BroccoliBot is Team 955's >>>>>>Ut enim quasi commodi ducimus dolorum corrupti. In veritatis quas mollitia qui. Aliquid incidunt et molestias est ea est. Et ea et cum rerum. Repellendus enim aut sequi.
+                    </p>
+                    <p>Quibusdam aut debitis sit ut voluptas consequatur quidem facilis. Aut natus voluptate beatae rerum autem accusamus molestiae. Id voluptate sint ducimus iusto et tenetur deleniti. Vero rerum quia dolorum commodi quibusdam vitae dolor. Quasi animi similique quia. Numquam nobis cum est deserunt ut repellendus quos.
+                    </p>
+                </section>
+            </div>
+        </div>
+        <div class="subpageBox">
+            <div class="row crossScreenImg" id="BotVideo">
+                <section class="col-md-12 centerV">
+                    <iframe width="875" height="515" src="https://www.youtube.com/embed/EI9ZJs56EJk" frameborder="0" allowfullscreen></iframe>
+                </section>
+            </div>
+            
+            <div class="row" id="BotDescription">
+                <section class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+                    <p>
+                    ...
+                    </p>
+                </section>
+                <section class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                     <p>
                     ...
                     </p>
                 </section>
             </div>
-            
-            
+            <div class="row crossScreenImg" id="BotDescription">
+                <section class="col-lg-6 col-lg-offset-3 col-md-6 col-md-offset-3 col-sm-12 col-xs-12">
+                    <img>
+                </section>
+            </div>
         </div>
-        
-        
         
         
         
