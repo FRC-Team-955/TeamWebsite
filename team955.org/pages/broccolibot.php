da30c3b431935e7b3725bb90df8f6b14150360ab
Miles Smith <milofarseer@comcast.net>
2017-11-14 21:51:11 -0800

    Content Update for brocbot

--- team955.org/pages/broccolibot.php
+++ team955.org/pages/broccolibot.php
@@ -20,9 +20,11 @@
                 </section>
                 <section class="col-xs-12 col-sm-6 col-md-6 col-lg-8">
                     <p>
-                    BroccoliBot is Team 955's >>>>>>Ut enim quasi commodi ducimus dolorum corrupti. In veritatis quas mollitia qui. Aliquid incidunt et molestias est ea est. Et ea et cum rerum. Repellendus enim aut sequi.
+                    Broccoli is difficult to mechanically harvest due to the way it grows; the head is concealed by a canopy of leaves. To deal with this issue, a plant breeding professor at Oregon State University developed a type of broccoli with an exerted head, which allowed for easier harvesting. Unfortunately, they could not find a way to market their broccoli in the agriculture industry due to the lack of harvesters built for such a type of broccoli. Upon learning about the issue, Peter Mes decided that it was the perfect opportunity for us, Crescent Valley High School Robotics Team 955, to display our STEM skills as well as gain real world work experience.
+
                     </p>
-                    <p>Quibusdam aut debitis sit ut voluptas consequatur quidem facilis. Aut natus voluptate beatae rerum autem accusamus molestiae. Id voluptate sint ducimus iusto et tenetur deleniti. Vero rerum quia dolorum commodi quibusdam vitae dolor. Quasi animi similique quia. Numquam nobis cum est deserunt ut repellendus quos.
+                    <p>
+                         He proposed the idea of Broccoli Bot, a robot built to mechanically harvest all types of broccoli using an advanced vision system, to Professor James Myers and Professor Ed Peachey of OSU. They were quick to accept the proposition. In addition to being an opportunity for the team, this project would fill a gap in the agricultural tech industry, as the current methods require expensive and plentiful manual labor.
                     </p>
                 </section>
             </div>
@@ -30,19 +32,19 @@
         <div class="subpageBox">
             <div class="row crossScreenImg" id="BotVideo">
                 <section class="col-md-12 ">
-                    <iframe width="80%" height="515" src="https://www.youtube.com/embed/EI9ZJs56EJk" frameborder="0" allowfullscreen></iframe>
+                    <iframe width="100%" height="515" src="https://www.youtube.com/embed/EI9ZJs56EJk" frameborder="0" allowfullscreen></iframe>
                 </section>
             </div>
             
             <div class="row" id="BotDescription">
                 <section class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                     <p>
-                    ...
+                    We created, developed, and wrote a computer program that utilizes a camera to find the exact distance of the machine’s blade from the broccoli’s crown. The camera detects the unique texture of the broccoli, and the computer software then determines the machine’s position relative to the broccoli’s crown in 3D space so that the blade knows precisely where to slice.
                     </p>
                 </section>
                 <section class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
                     <p>
-                    ...
+                    In the future, we hope to create Prototype 5 (P5) which can not only harvest broccoli, but also collect it. Through this, we hope to create a solution that the agriculture industry is not only satisfied with, but is also affordable to the everyday broccoli farmer.
                     </p>
                 </section>
             </div>
